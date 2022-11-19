4f656486b24c13584e58cc91d29fbdbb77741c6d
曾大海 <zsea@qq.com>
2022-11-19 23:55:36 +0800

    添加文档

--- index.ts
+++ index.ts
@@ -51,8 +51,8 @@ import { CryptoManager } from './fs/icrypto';
         throw new Error("inode文件初始化失败。");
     }
     let fogFs = new FogFileSystem(inodeManager, configure.blockSize);
-    if (await server.setFileSystemAsync(configure.path, fogFs)) {
-        logger.info(`文件系统挂载成功 ${configure.path}`);
+    if (await server.setFileSystemAsync(configure.path||"/", fogFs)) {
+        logger.info(`文件系统挂载成功 ${configure.path||"/"}`);
     }
     else {
         logger.error("文件系统挂载失败");
@@ -64,7 +64,7 @@ import { CryptoManager } from './fs/icrypto';
     })
     express.use(webdav.extensions.express('/', server));
     let httpServer =express.listen(Number(process.env.PORT || process.env["WEBPORT"] || 3000),()=>{
-        httpServer.setTimeout(configure.timeout);
+        httpServer.setTimeout(configure.timeout||0);
         logger.info(`服务启动成功`, httpServer.address());
     })
 })();
